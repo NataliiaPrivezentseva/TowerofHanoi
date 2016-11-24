7054684dba954a2770ce45c9fe9efc71c8ff088a
Nataliia Privezentseva <nataliia.privezentseva@gmail.com>
2016-11-24 21:29:42 +0100

    correct discs behavior in putDisc method

--- src/main/java/Peg.java
+++ src/main/java/Peg.java
@@ -3,37 +3,38 @@ import java.util.LinkedList;
 import java.util.NoSuchElementException;
 
 class Peg {
-    private final Deque<Disc> discs;
+    private final Deque<Disc> peg;
 
     Peg() {
-        discs = new LinkedList<>();
+        peg = new LinkedList<>();
     }
 
     void putDisc(Disc disc) {
         if (disc == null) {
-            throw new NullPointerException("You haven't any discs for putting");
+            throw new NullPointerException("You haven't any peg for putting");
         }
 
-        if (!discs.isEmpty() && discs.getFirst().getDiscNumber() < disc.getDiscNumber()) {
+        if (!peg.isEmpty() && peg.getFirst().getDiscNumber() <= disc.getDiscNumber()) {
             throw new IllegalStateException("You can't put a disc with bigger discNumber " +
-                    "on the disc with smaller discNumber");
+                    "on the disc with smaller discNumber. Also you can't put disc on the" +
+                    "disc with the same discNumber");
         }
 
-        discs.addFirst(disc);
+        peg.addFirst(disc);
     }
 
     Disc takeDisc() {
-        if (discs.isEmpty()) {
+        if (peg.isEmpty()) {
             throw new NoSuchElementException("You can't take a disk from an empty peg");
         }
-        return discs.removeFirst();
+        return peg.removeFirst();
     }
 
     void printPegState() {
-        if (discs.isEmpty()) {
-            System.out.println(" don't contains any discs");
+        if (peg.isEmpty()) {
+            System.out.println(" don't contains any peg");
         } else {
-            System.out.println(" contains following discs: " + discs);
+            System.out.println(" contains following peg: " + peg);
         }
     }
 }
